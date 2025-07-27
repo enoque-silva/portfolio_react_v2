6ec37b0b87c6e087fe1e20c54a92e1b0b48a48f6
enoque <eu.enoqueribeiro@gmail.com>
2025-07-27 18:17:32 -0300

    Construindo a estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/enoque-silva.png" />
+    <Titulo>Enoque Ribeiro</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nesciunt
+      aliquid hic corrupti nisi ipsam veritatis perspiciatis harum facere!
+      Maiores veniam itaque ex nisi! Delectus placeat excepturi dicta quod
+      accusantium.
+    </p>
+  </section>
+)
+
+export default Sobre
