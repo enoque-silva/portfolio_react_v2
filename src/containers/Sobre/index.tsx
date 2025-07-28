0020bad0ff8000363fc4d8e298249af9b2004252
enoque <eu.enoqueribeiro@gmail.com>
2025-07-28 00:09:42 -0300

    adiciona estilo para a seção Github no componente Sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,15 +1,21 @@
 import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
-    <Titulo fontSize={20}>Sobre</Titulo>
+    <Titulo fontSize={20}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nesciunt
       aliquid hic corrupti nisi ipsam veritatis perspiciatis harum facere!
       Maiores veniam itaque ex nisi! Delectus placeat excepturi dicta quod
       accusantium.
     </Paragrafo>
+
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=enoque-silva&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=enoque-silva&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  margin-top: 32px;
+  margin-bottom: 64px;
+  height: 100%;
+
+  img {
+    height: 157px;
+  }
+`
