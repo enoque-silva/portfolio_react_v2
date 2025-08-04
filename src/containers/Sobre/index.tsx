b0cbaad25cea0a0cc20da9b519a683e875cc0e47
enoque <eu.enoqueribeiro@gmail.com>
2025-08-04 19:53:49 -0300

    fix: paragrafos sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,24 +1,25 @@
 import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao } from '../Sidebar/styles'
 import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
     <Titulo fontSize={20}>Sobre mim</Titulo>
-    <Paragrafo tipo="principal">
+    <Descricao tipo="principal">
       Sou graduando em Engenharia da Computação e entusiasta em criar
       experiências digitais que unem design, funcionalidade e performance. Em
       formação em Desenvolvimento Fullstack pela EBAC, atuo na construção de
       interfaces modernas e responsivas, sempre com foco na usabilidade e na
       experiência do usuário.
-    </Paragrafo>
+    </Descricao>
     <br />
-    <Paragrafo tipo="principal">
+    <Descricao tipo="principal">
       Tenho facilidade em aprender novas tecnologias, trabalhar em equipe e
       transformar desafios em soluções criativas. Meu objetivo é contribuir para
       projetos inovadores, entregando resultados que gerem impacto real para
       empresas e usuários.
-    </Paragrafo>
+    </Descricao>
 
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=enoque-silva&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
